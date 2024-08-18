413671d105f04192793c7e85e691ff38675f8165
Samuel Kendall <kinguser981@gmail.com>
2024-08-18 12:41:35 +0330

    OneUISounds Magisk

---
+++ README.md
@@ -0,0 +1,14 @@
+# One UI 6.1 Sounds 2024
+
+One UI 6.1 High quality system sounds (notifications, Spen, rigtones and UI sounds) 
+files taken from the Galaxy S22 Ultra SM-S908B stock ROM (S908BXXSAEXE3) based on One UI 6.1
+Some UI sounds may not be replaced if your ROM has different filenames.
+Fully compatible with Samsung Galaxy devices with stock ROM.
+
+# Changelog
+## v6.1
+- Added new One UI Sounds 
+- Added old One UI Sounds 
+- Added Spen UI sound
+- Added SoundTheme
+
---
+++ install.sh
@@ -0,0 +1,47 @@
+#
+# Magisk Module Installer Script
+#
+
+SKIPMOUNT=false
+
+PROPFILE=false
+
+POSTFSDATA=false
+
+LATESTARTSERVICE=false
+
+
+REPLACE_EXAMPLE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+
+REPLACE="
+"
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "*      One UI  6.1 Sounds  *"
+  ui_print "*******************************"
+  ui_print "*     Module by SuperSU   *"
+  ui_print "*******************************"
+}
+
+
+on_install() {
+
+  ui_print "- Extracting module files"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+
+set_permissions() {
+
+  set_perm_recursive $MODPATH 0 0 0755 0644
+
+
+}
+
